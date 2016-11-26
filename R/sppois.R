6a1c253cf23a8e4a605289bffc4fd8b816a65325
Greg Macfarlane <gregmacfarlane@gmail.com>
2016-11-26 11:05:55 -0500

    Add package documentation

---
+++ R/sppois.R
@@ -0,0 +1,10 @@
+#' sppois: A package for estimating spatial poisson regressions.
+#'
+#' The spppois package provides functions for estimating and summarizing output
+#' from spatial autoregressive Poisson count models. The primary
+#' estimation fuction is \link{sarpoisson}
+#'
+#' @docType package
+#' @name sppois
+#'
+NULL
